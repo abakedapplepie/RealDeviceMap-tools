d035ba134757a64791af348d48c4fabbad4190b0
Florian Kostenzer <florian.kostenzer@icloud.com>
2021-02-19 13:19:15 +0100

    Update config.env.php

--- config/config.env.php
+++ config/config.env.php
@@ -1,11 +1,11 @@
 <?php
 
 //Configuration options
-define('DB_HOST', getenv("DB_HOST") ?? "127.0.0.1");
-define('DB_USER', getenv("DB_USER") ?? "rdmuser");
-define('DB_PSWD', getenv("DB_PSWD") ?? "password");
-define('DB_NAME', getenv("DB_NAME") ?? "rdmdb");
-define('DB_PORT', getenv("DB_PORT") ?? 3306);
-define('OWN_TS', getenv("OWN_TS") ?? "https://IP:PORT/tile/STYLE/{z}/{x}/{y}/1/png");
+define('DB_HOST', getenv("DB_HOST") ?: "127.0.0.1");
+define('DB_USER', getenv("DB_USER") ?: "rdmuser");
+define('DB_PSWD', getenv("DB_PSWD") ?: "password");
+define('DB_NAME', getenv("DB_NAME") ?: "rdmdb");
+define('DB_PORT', getenv("DB_PORT") ?: 3306);
+define('OWN_TS', getenv("OWN_TS") ?: "https://IP:PORT/tile/STYLE/{z}/{x}/{y}/1/png");
 
 ?>
